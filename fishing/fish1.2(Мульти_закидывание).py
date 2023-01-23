7d98cfefbf19aaff4994c862c9061f963ee2fdf0
demin1078 <43730344+demin1078@users.noreply.github.com>
2023-01-23 08:15:21 +0300

    Eating food

--- fishing/fish1.2(Мульти_закидывание).py
+++ fishing/fish1.2(Мульти_закидывание).py
@@ -1,6 +1,3 @@
-import random
-import time
-
 import cv2 as cv
 import keyboard
 import numpy as np
@@ -10,18 +7,28 @@ from PIL import ImageGrab
 from os import listdir
 from os.path import isfile, join
 import os
-import re
+import time, datetime
 
-THRESHOLD_CATCH = 0.4
-THRESHOLD_BAR = 0.05
+THRESHOLD_CATCH = 0.3 # 0.4 for ussual # 0.3 for sea
+THRESHOLD_BAR = 0.1
 PHOTO_PTH = "C:\\Users\\dimag\\differentProjects\\fishing\\PHOTOES"
+
 DEBUG = 0
 
+SEA_MOD = 0
+SEA_ARR_NUMB = 6
+SEA_ARR = [0] * SEA_ARR_NUMB #5 len
+
+fl = open("food_ts.txt")
+FOOD_TIME_LAST = fl.read().split(";")[-2]
+FOOD_TIME_LAST = datetime.datetime.strptime(FOOD_TIME_LAST, "%Y-%m-%d %H:%M:%S")
+TM_DEL_30 = datetime.timedelta(minutes=30)
+fl.close()
 
 time_between_games = 7
 
-x_cursor = [1316]
-y_cursor = [403]
+x_cursor = [560]
+y_cursor = [113]
 
 fishing_rod = (801, 208)
 
@@ -38,6 +45,7 @@ abort = cv.imread('reconnecting.png', cv.IMREAD_UNCHANGED)
 hooks = []
 mypath = "hooks/"
 all_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
 for fl in all_files:
     pth = mypath+fl
     hk_img = cv.imread(pth, 0)
@@ -93,8 +101,8 @@ def catching_fish():
         result = cv.matchTemplate(haystack_img, needle_img, cv.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
         if DEBUG:
+            print(min_val, max_val, i, min_loc, max_loc)
             screenshot.save(f"PHOTOES/bar{i}.png")
-            print(min_val, max_val, i)
         if max_val <= THRESHOLD_BAR or i >= 1700:
             pyautogui.mouseUp(button='left')
             caught_fish += 1
@@ -106,7 +114,6 @@ def catching_fish():
             pixel = screenshot.getpixel(coord)
             if pixel[0] > 130 and pixel[1] > 130 and pixel[2] > 130 and is_pressed == 0:
                 is_pressed = 1
-
                 pyautogui.mouseDown(button='left')
                 break
 
@@ -115,9 +122,7 @@ def catching_fish():
             pixel = screenshot.getpixel(coord)
             if pixel[0] > 130 and pixel[1] > 130 and pixel[2] > 130:
                 is_pressed = 0
-
                 pyautogui.mouseUp(button='left')
-
                 break
 def pixel_coords():
     time.sleep(0.1)
@@ -149,33 +154,29 @@ def start_fishing(place_numb):
     while True:
         screenshot = ImageGrab.grab(bbox=cursor_coord)
         haystack_img = cv.cvtColor(np.array(screenshot.copy()), cv.COLOR_BGR2GRAY)
-
         maxs = []
+        mins = []
         for hook in hooks:
             mtch = cv.matchTemplate(haystack_img, hook, cv.TM_CCOEFF_NORMED)
-            _, max_val, _, _ = cv.minMaxLoc(mtch)
+            min_val, max_val, _, _ = cv.minMaxLoc(mtch)
             maxs.append(max_val)
+            mins.append(min_val)
         average = sum(maxs)/len(maxs)
 
-        # if (pixel_color[2] > pixel_color[1] and pixel_color[2] > pixel_color[0] and diff > 40)\
-        #         or (pixel_color[1] >110 and pixel_color[0]>110):
-
-        if DEBUG: (average, i)
-        if average <= THRESHOLD_CATCH:
+        SEA_ARR[int(i % SEA_ARR_NUMB)] = 1 if average < THRESHOLD_CATCH else 0
+        if DEBUG: print(average, i, max(maxs), min(mins))
+        if ((average <= THRESHOLD_CATCH  or max(maxs) < 0.5)and SEA_MOD == 0) or (SEA_MOD == 1 and all(SEA_ARR)):
             if DEBUG: screenshot.save(f'{PHOTO_PTH}\поймал{i}.png')
-            i += 1
             pyautogui.mouseUp(button='left')
             time.sleep(0.1)
             return 1
-
         else:
-            i += 1
             if DEBUG: screenshot.save(f'{PHOTO_PTH}\Попловок{i}.png')
+        i += 1
 
 
 def change_fl_names():
-    mypath = "PHOTOES/"
-    all_files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+    all_files = [f for f in listdir(mypath) if isfile(join(PHOTO_PTH, f))]
     i = 0
     for fl in all_files:
         if "поймал" in fl:
@@ -185,10 +186,40 @@ def change_fl_names():
                 os.rename(mypath + fl, mypath + new_file)
     print("Названия заменины")
 
+# def clean_photo():
+#     all_files = [f for f in listdir(PHOTO_PTH) if isfile(join(PHOTO_PTH, f))]
+#     print(all_files)
+#     print(PHOTO_PTH)
+#     for i in all_files:
+#         os.remove(PHOTO_PTH)
+#         print(PHOTO_PTH + i)
+#     print("Photo were deleted")
+
+def eat_food(FOOD_TIME_LAST):
+    print("eat food")
+    tm_now = datetime.datetime.now()
+    print(tm_now - TM_DEL_30)
+    print(FOOD_TIME_LAST)
+    if tm_now - TM_DEL_30 >= FOOD_TIME_LAST:
+        print("time")
+        pyautogui.press('2')
+        time.sleep(0.2)
+        pyautogui.press('2')
+
+        # Write new time in file
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        file1 = open("food_ts.txt", "a")  # append mode
+        file1.write(f"{now};")
+        file1.close()
+
+        FOOD_TIME_LAST =  datetime.datetime.now()
+
+    return FOOD_TIME_LAST
+
+
 
 #Основной цикл
 while True:
-
     caught_fish = 0
     if keyboard.is_pressed('p'):
         print("Была нажата p")
@@ -204,6 +235,9 @@ while True:
             start_fishing(0)
             catching_fish()
             time.sleep(5)
+            now = datetime.datetime.now()
+            if now - TM_DEL_30 >= FOOD_TIME_LAST:
+                FOOD_TIME_LAST = eat_food(FOOD_TIME_LAST)
 
     if keyboard.is_pressed('i'):
         catch_fish()
